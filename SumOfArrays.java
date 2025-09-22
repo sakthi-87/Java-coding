85230f46dfea09435e82313aa81a24c789ff427a
sakthi-87 <sakthisureshkumar575@gmail.com>
2025-09-22 15:02:54 +0530

    Added SumOfArrays.java

---
+++ SumOfArrays.java
@@ -0,0 +1,12 @@
+public class sumOfArrays {
+
+	public static void main(String[] args) {
+		int arr[]={3,6,2,8,9};
+		int sum=0;
+		for(int num : arr){
+			sum+=num;
+		}
+System.out.println("Sum of Arrays:" +sum);
+	}
+
+}
